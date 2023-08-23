d0b4520e192fbcab83645d564e27068cac13a6c2
Felipe Beserra <felipebeserra25@gmail.com>
2023-08-23 08:52:44 -0300

    Adding repos functions

--- scripts/lib/apt.sh
+++ scripts/lib/apt.sh
@@ -23,6 +23,29 @@ function apt_install {
   fi
 }
 
+# Not the best way to do it, but probably the easiest
+function apt_repo {
+  if already_exists $1; then
+    echo "${2} repo already added"
+  else
+    echo "Adding Repo: ${2}..."
+    sudo add-apt-repository -y $2
+    sudo apt update
+  fi
+}
+
+# apt_add <command> <repo_string> <key_url>
+function apt_add {
+  if already_exists $1; then
+    echo "${2} repo already added"
+  else
+    echo "Adding Repo: ${2}..."
+    sudo curl -o "/usr/share/keyrings/${1}-keyring.gpg" $3
+    echo "deb [signed-by=/usr/share/keyrings/${1}-keyring.gpg] ${2}" | sudo tee /etc/apt/sources.list.d/${1}.list
+    sudo apt update
+  fi
+}
+
 function apt_remove {
   if already_exists $1; then
     echo "Removing: ${1}..."
