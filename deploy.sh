f23c81287339e50c6a31b8aedf8a236c7a2068e7
Felipe Beserra <felipebeserra25@gmail.com>
2023-08-23 08:54:57 -0300

    Fixed sudo check

--- deploy.sh
+++ deploy.sh
@@ -2,15 +2,16 @@
 SCRIPT=$(readlink -f "$0")
 SCRIPTPATH=$(dirname "$SCRIPT")
 
-# Check if script is run with sudo priviligies 
-if [ $(sudo -n uptime 2>&1|grep "load"|wc -l) -eq 0 ]
-  then printf "Please run with sudo\n"
-  exit
-fi
+# Source util functions
+. $SCRIPTPATH/scripts/lib/utils.sh
+
+# Check if script has su priviligies
+check_sudo # from core.sh
 
 # Copy dotfiles configs to home folder
-cp -r $(BASEDIR)/user_config/.* ~/
+cp -r $SCRIPTPATH/user_config/.* ~/
 
-# Configure POP_OS Shell
+# Configure POP_OS Shell, requires reboot
 # Configure Terminal look
 # Configure GPG git key
+# sensors-detect
